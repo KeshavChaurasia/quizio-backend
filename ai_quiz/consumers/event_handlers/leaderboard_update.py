11817119bd3d04aeb9d92f41456ce602cccece11
kaushu42 <kaushu42@gmail.com>
2024-12-28 15:20:15 +0000

    leaderboard update

--- ai_quiz/consumers/event_handlers/leaderboard_update.py
+++ ai_quiz/consumers/event_handlers/leaderboard_update.py
@@ -12,7 +12,7 @@ class LeaderboardUpdateEventHandler(BaseEventHandler):
     async def handle(self, event, consumer):
         game = await Game.aget_current_game_for_room(consumer.room_code)
         try:
-            leaderboard = await Leaderboard.objects.get(game=game)
+            leaderboard = await Leaderboard.objects.aget(game=game)
         except Leaderboard.DoesNotExist:
             self.consumer.send_error("Leaderboard not found.")
             return
